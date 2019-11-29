5ec8d0f986cc61b67efba93c78ca8a1c1477bd5f
luke <luke.luo@gmail.com>
2019-11-29 17:47:08 -0500

    create IP list

--- app/get_all_ip.sh
+++ app/get_all_ip.sh
@@ -1,4 +1,4 @@
-nslookup app | grep Address | awk -F: '{print $2}' | awk -F' ' '{print $1}' > /tmp/all_ip.txt
+nslookup app | grep Address | awk -F: '{print $2}' | awk -F' ' '{print $1}' > /www/all_ip.txt
 #nginx -g 'daemon off'
 #nginx -s start
 
--- app/loop.sh
+++ app/loop.sh
@@ -1,3 +1,4 @@
+/tmp/get_all_ip.sh
 nginx
 trap 'exit 0' SIGTERM
 while true; do :; done
