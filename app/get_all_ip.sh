8c9921f0f9ce34fed8c98529988735a8622d7b3f
luke <luke.luo@gmail.com>
2019-11-30 09:04:56 -0500

    Show current connected Nginx IP

--- app/get_all_ip.sh
+++ app/get_all_ip.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+
+rm -f /www/my_ip.txt
+MYIP=$(ip route get 1 | awk '{print $NF;exit}')
+echo $MYIP > /www/my_ip.txt
+
+
 rm -f /mydata/all_ip.txt
 nslookup app | grep Address | awk -F: '{print $2}' | awk -F' ' '{print $1}' > /mydata/all_ip.txt
 ln -s /mydata/all_ip.txt /www/all_ip.txt
