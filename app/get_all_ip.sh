a15c220a17c3f699fa5bfede91ff3fdb411ab772
luke <luke.luo@gmail.com>
2019-11-29 23:37:50 -0500

    dynamic update IPs

--- app/get_all_ip.sh
+++ app/get_all_ip.sh
@@ -1,4 +1,8 @@
-nslookup app | grep Address | awk -F: '{print $2}' | awk -F' ' '{print $1}' > /www/all_ip.txt
+#!/bin/sh
+
+rm -f /mydata/all_ip.txt
+nslookup app | grep Address | awk -F: '{print $2}' | awk -F' ' '{print $1}' > /mydata/all_ip.txt
+ln -s /mydata/all_ip.txt /www/all_ip.txt
 #nginx -g 'daemon off'
 #nginx -s start
 
--- app/loop.sh
+++ app/loop.sh
@@ -1,4 +1,20 @@
+#!/bin/sh
+
 /tmp/get_all_ip.sh
 nginx
-trap 'exit 0' SIGTERM
+
+#ip route get 1 | awk '{print $NF;exit}'
+cleanup()
+{
+  MYIP=$(ip route get 1 | awk '{print $NF;exit}')
+  echo $MYIP
+  sed -i -e "s/$MYIP//g" /mydata/all_ip.txt
+  sed -i '/^$/d' /mydata/all_ip.txt
+  echo "Catch the shutdown event" > /mydata/evt.txt
+  exit 170
+}
+
+#trap 'exit 0' SIGTERM
+
+trap cleanup SIGKILL SIGTERM
 while true; do :; done
