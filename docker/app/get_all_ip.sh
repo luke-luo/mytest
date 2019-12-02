afe9df076d2eac4a6b1c633ee24e84ba1d21d5bc
luke <luke.luo@gmail.com>
2019-12-01 22:16:20 -0500

    temp - 1

--- docker/app/get_all_ip.sh
+++ docker/app/get_all_ip.sh
@@ -9,8 +9,3 @@ rm -f /mydata/all_ip.txt
 nslookup app | grep Address | awk -F: '{print $2}' | awk -F' ' '{print $1}' > /mydata/all_ip.txt
 ln -s -f /mydata/all_ip.txt /www/all_ip.txt
 
-ln -s -f /mydata/all_ip.txt /www/all_ip.txt
-
-#nginx -g 'daemon off'
-#nginx -s start
-
